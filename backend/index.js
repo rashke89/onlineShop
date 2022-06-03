83965e6a0497e53a2183df953e643a3d458f42cf
Nex996 <neks96@gmail.com>
2022-06-03 20:59:26 +0200

    Set update user and delete user

--- backend/index.js
+++ backend/index.js
@@ -6,13 +6,16 @@ const Users = require('./models/userModel');
 const serverConfig = require('./config/serverConfig');
 
 const app = express();
+
+//CONNECT TO MONGO DB
 mongoose.connect(dbConfig.MONGODB_URL)
     .then(data => console.log('MONGO DB is connected.'))
     .catch(err => console.log(`Error while connecting to MONGO DB: ${err}`));
-
+//CONNECT TO MONGO DB NED
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+//LOGIN
 app.post('/api/login', (req, res) => {
     const reqBody = req.body;
 
@@ -39,13 +42,15 @@ app.post('/api/login', (req, res) => {
 
     });
 });
+//LOGIN END
 
+
+//REGISTER
 app.post('/api/register', async (req, res) => {
     const reqBody = req.body;
     // console.log('reg user data:', reqBody);
 
     Users.findOne(reqBody,  async (err, data) => {
-        console.log(data);
         if (err) {
             const errorMsg = `Error on register user: ${err}`;
             console.log(errorMsg);
@@ -65,11 +70,56 @@ app.post('/api/register', async (req, res) => {
     });
 
 });
+//REGISTER END
+
+//Server TEST
+app.get("/",(req, res)=>{
+    res.send("Welcome to server");
+})
+
+
+//delete user by email
+app.delete("/api/user/:email",(req, res)=>{
+    const params = req.params.email
+    Users.deleteOne({email:params},null,(error)=>{
+        if (error) throw error
+        res.send("User deleted")
+    })
+})
+
+//get all users
+app.get("/api/users",(req, res)=>{
+    Users.find((error, result)=>{
+        if(error) throw error;
+        res.send(result)
+    })
+})
+
+//get one user by username
+app.get("/api/user/:username",(req, res)=>{
+    const param = req.params.username
+    Users.find({"username":param},(error, result)=>{
+        if(error) throw error;
+        res.send(result)
+    })
+})
+
+//update user email by username
+app.put("/api/user/:username",(req, res)=>{
+    const param = req.params.username
+    const query = req.query
+
+    Users.updateOne({"username":param},{email:query.email,isAdmin:query.admin},null,(error, result)=>{
+        if(error) throw error
+        res.send(result)
+    })
+})
 
 // get one user: GET method, URL: 'api/user/:username', {username} is URL param
 // get all users: GET method, URL: 'api/users',
 // edit user: PUT method, URL: 'api/user/:username',
 
+//SERVER LISTENING
 app.listen(serverConfig.port, err => {
     if (err) {
         console.log(err);
