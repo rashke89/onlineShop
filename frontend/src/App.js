1eddad7bc6d836a7812ea93b68cac9c0ac1b095c
mbondokic <milos.bondokic@gmail.com>
2022-06-08 22:02:48 +0200

    bootstrap js included

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import AuthPage from "./pages/authPage";
 import axios from 'axios';
-import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import ShopPage from "./pages/ShopPage";
 import AboutUs from "./pages/AboutUs";
 import ContactPage from "./pages/ContactPage";
@@ -13,14 +13,7 @@ axios.defaults.baseURL = 'http://localhost:4000';
 function App() {
 	return (
 		<div className="App">
-			{/*<header className="App-header">*/}
-			{/*  <AuthPage />*/}
-			{/*</header>*/}
-
 			<BrowserRouter>
-				{/*<Link to='/shop'>Shop</Link>*/}
-				{/*<Link to='/about-us'>About us</Link>*/}
-				{/*<Link to='/contact'>Contact</Link>*/}
 				<Navbar/>
 				<Routes>
 					<Route path='/' element={<AuthPage/>}/>
