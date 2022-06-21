b398303524789ffacba650b063cf9d3766d10626
Nex996 <neks96@gmail.com>
2022-06-21 20:20:12 +0200

    Add container and row in My Ads

--- frontend/src/pages/MyAds/MyAds.js
+++ frontend/src/pages/MyAds/MyAds.js
@@ -2,11 +2,19 @@ import React from 'react';
 import {Link} from "react-router-dom";
 
 function MyAds(props) {
+
+
 	return (
 		<div>
 			<h2 className="text-center my-5">My Ads</h2>
 			<div className="btn-wrapper d-flex justify-content-center align-items-center">
 				<Link to="/add-product" className="btn btn-primary">Add product</Link>
+			</div>
+			<div className="container">
+				<div className="row">
+
+				</div>
+
 			</div>
 		</div>
 	);
