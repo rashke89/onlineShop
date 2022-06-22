7288fea711a9f6ea6062cb387d215574fc4b8010
Rados <rashke89@gmail.com>
2022-06-22 19:23:50 +0200

    cart finished

--- frontend/src/components/ShopCart/ShopCart.js
+++ frontend/src/components/ShopCart/ShopCart.js
@@ -1,11 +1,13 @@
 import React, {useEffect, useRef} from "react";
-import {FaCartPlus} from "react-icons/fa";
+import {FaCartPlus, FaMinusCircle, FaPlusCircle, FaTrashAlt} from "react-icons/fa";
 import "./shop-cart.scss";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {removeItem, handleCount} from "../../redux/cartSlice";
 
 function ShopCart() {
     const {cart} = useSelector(state => state.cartStore);
     const shopCartWrapperRef = useRef();
+    const dispatch = useDispatch();
 
     useEffect(() => {
         console.log(cart);
@@ -19,11 +21,31 @@ function ShopCart() {
         }
     }, [cart]);
 
+    const removeItemFromCart = (index) => {
+        dispatch(removeItem(index))
+    };
+
+    const handleShopCartCount = (index, isIncrement) => {
+        dispatch(handleCount({index, isIncrement}))
+    };
+
     const shopCartSumLayout = () => {
         return cart.map((item, index) => {
-            return <div className="shop-cart-item">
-                {item.title}
-                {item?.count}
+            return <div className="shop-cart-item row mt-3" key={index}>
+                <div className="col-md-3">
+                    <img src={item.image} alt=""/>
+                </div>
+                <div className="col-md-8">
+                    <h5>{item.title}</h5>
+
+                    {item.count > 1 && <p>Count: <FaMinusCircle className="mx-2" onClick={() => handleShopCartCount(index, false)} /> {item.count}
+                    <FaPlusCircle className="mx-2" onClick={() => handleShopCartCount(index, true)}/></p>}
+
+                    <p className="fw-bold">{item.price * item.count} $</p>
+                </div>
+                <div className="col-md-1 remove-icon-wrapper">
+                    <FaTrashAlt onClick={() => {removeItemFromCart(index)}} />
+                </div>
             </div>
         })
     }
