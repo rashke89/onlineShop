3f1793dfa5a5529e19f2f77636c2c887e92e38a0
mbondokic <milos.bondokic@gmail.com>
2022-06-23 15:46:22 +0200

    My ads fix

--- frontend/src/pages/MyAds/MyAds.js
+++ frontend/src/pages/MyAds/MyAds.js
@@ -3,6 +3,8 @@ import {Link} from "react-router-dom";
 import ShopService from "../../services/shopService";
 import MyAd from "../../components/MyAd/MyAd";
 
+// Push fix
+
 function MyAds(props) {
 	const [products, setProducts] = useState([]);
 
