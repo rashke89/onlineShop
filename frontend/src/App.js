32b1dc2d2090f0a02664e2700b9a7567d8107fe2
adrijanasavic <adrijana.jovicic@gmail.com>
2022-06-23 06:35:17 +0200

    added aos animate

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,20 @@
 import axios from "axios";
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import AuthPage from "./pages/AuthPage/AuthPage";
 import './assets/scss/base.scss';
 
-import {Routes, Route, useNavigate} from "react-router-dom"
+import { Routes, Route, useNavigate } from "react-router-dom"
 import Shop from "./pages/Shop/Shop";
 import About from "./pages/About/About";
 import Contact from "./pages/Contact/Contact";
 import Navigation from "./components/navigation/Navigation";
 import Home from "./pages/Home/Home";
 import CookisPopup from "./components/cookies/CookiesPopup";
-import {useDispatch, useSelector} from "react-redux";
-import {setUser} from "./redux/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { setUser } from "./redux/userSlice";
 import ActivateUserPage from "./pages/ActivateUserPage/ActivateUserPage";
 import AdPage from "./pages/AdPage/AdPage";
-import {routeConfig} from "./config/routeConfig";
+import { routeConfig } from "./config/routeConfig";
 import { setAcceptCookies } from "./redux/cookiesSlice";
 
 export const IsLoggedContext = React.createContext();
@@ -24,37 +24,34 @@ function App() {
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const [showModal, setShowModal] = useState(false);
-    const { acceptCookies } = useSelector(state => state.cookiesStore )
+    const { acceptCookies } = useSelector(state => state.cookiesStore)
 
     useEffect(() => {
-        if (!localStorage.hasOwnProperty('user')){
+        if (!localStorage.hasOwnProperty('user')) {
             // navigate('/auth');
         } else {
             dispatch(setUser(JSON.parse(localStorage.getItem('user'))))
-          
         }
         if (localStorage.hasOwnProperty('accept')) {
             dispatch(setAcceptCookies())
         }
         setTimeout(() => {
             setShowModal(true)
-        }, 5000 )
+        }, 5000)
     }, []);
-
 
     return (
         <div className="main-wrapper">
             <Navigation />
-
-            {!acceptCookies && showModal ? <CookisPopup />  : null }
+            {!acceptCookies && showModal ? <CookisPopup /> : null}
             <Routes>
-                <Route path={routeConfig.HOME.url} element={<Home/>}/>
-                <Route path={routeConfig.SHOP.url} element={<Shop/>}/>
-                <Route path={routeConfig.AD_SHOP.url} element={<AdPage/>}/>
-                <Route path={routeConfig.ABOUT.url} element={<About/>}/>
-                <Route path={routeConfig.CONTACT.url} element={<Contact/>}/>
-                <Route path={routeConfig.AUTH.url} element={<AuthPage/>}/>
-                <Route path={routeConfig.USER_ACTIVATE.url} element={<ActivateUserPage/>}/>
+                <Route path={routeConfig.HOME.url} element={<Home />} />
+                <Route path={routeConfig.SHOP.url} element={<Shop />} />
+                <Route path={routeConfig.AD_SHOP.url} element={<AdPage />} />
+                <Route path={routeConfig.ABOUT.url} element={<About />} />
+                <Route path={routeConfig.CONTACT.url} element={<Contact />} />
+                <Route path={routeConfig.AUTH.url} element={<AuthPage />} />
+                <Route path={routeConfig.USER_ACTIVATE.url} element={<ActivateUserPage />} />
             </Routes>
         </div>
     );
--- frontend/src/components/cookies/CookiesPopup.js
+++ frontend/src/components/cookies/CookiesPopup.js
@@ -1,43 +1,69 @@
-import React, { useState } from 'react'
-import { useDispatch } from 'react-redux';
-import {setAcceptCookies} from '../../redux/cookiesSlice'
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setAcceptCookies } from "../../redux/cookiesSlice";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export default function CookisPopup() {
-
     const [modalStyle, setModalStyle] = useState({
-        display: 'block'
-    })
+        display: "block",
+    });
 
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
     const onClickHandler = (e) => {
         if (e.target.name === "accept") {
-            localStorage.setItem("accept", true)
-            dispatch(setAcceptCookies())
+            localStorage.setItem("accept", true);
+            dispatch(setAcceptCookies());
         } else {
-            setModalStyle({ display: 'none' });
-    
+            setModalStyle({ display: "none" });
         }
-    }
-  
-  return (
-      <div>
-          <div className="modal" tabIndex="-1" style={modalStyle}> 
-              <div className="modal-dialog">
-                  <div className="modal-content">
-                      <div className="modal-header">
-                          <h5 className="modal-title">Modal title</h5>
-                          <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={onClickHandler}></button>
-                      </div>
-                      <div className="modal-body">
-                          <p>This website uses cookies.</p>
-                      </div>
-                      <div className="modal-footer">
-                          <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" name="upAccept" onClick={onClickHandler}>Close</button>
-                          <button type="button" className="btn btn-primary" name="accept" onClick={onClickHandler}>Accept</button>
-                      </div>
-                  </div>
-              </div>
-          </div>
-    </div>
-  )
+    };
+
+    useEffect(() => {
+        AOS.init();
+        AOS.refresh();
+    }, []);
+
+    return (
+        <div>
+            <div
+                className="modal"
+                tabIndex="-1"
+                style={modalStyle}
+                data-aos="fade-down"
+                data-aos-easing="linear"
+                data-aos-duration="1500">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <button
+                                type="button"
+                                className="btn-close"
+                                data-bs-dismiss="modal"
+                                aria-label="Close"
+                                onClick={onClickHandler}>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            <p>
+                                Our website uses cookies to improve your experience.
+                                <br />
+                                By continuing to use our site you consent to the use of cookies.
+                            </p>
+                        </div>
+                        <div className="modal-footer">
+                            {/* <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" name="upAccept" onClick={onClickHandler}>Close</button> */}
+                            <button
+                                type="button"
+                                className="btn btn-primary"
+                                name="accept"
+                                onClick={onClickHandler}>
+                                Accept
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
