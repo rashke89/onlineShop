2499d287f5f67c4a8911babc32081ca95ad5b05c
Nex996 <neks96@gmail.com>
2022-06-28 14:19:36 +0200

    Create config for routing

---
+++ frontend/src/config/routeConfig.js
@@ -0,0 +1,41 @@
+import myProduct from "../components/myProduct/MyProduct";
+
+export const routeConfig={
+    HOME:{
+        url:'/'
+    },
+    SHOP:{
+        url:'/shop'
+    },
+    SHOP_PRODUCT:{
+        url:'/shop/product/:productId',
+        realUrl:(productId)=>`/shop/product/${productId}`
+    },
+    ABOUT:{
+        url:'/about'
+    },
+    CONTACT:{
+        url:'/contact'
+    },
+    AUTH:{
+        url:'/auth'
+    },
+    USER_ACTIVATE:{
+        url:'/user-activate/:id',
+
+    },
+    MY_PRODUCTS:{
+        url:'/myProducts'
+    },
+    ADD_PRODUCT:{
+        url:'/addProduct'
+    },
+    EDIT_PRODUCT:{
+        url:'/product/edit/:myProductId',
+        realUrl:(myProductId)=>`/product/edit/${myProductId}`
+    },
+    DELETE_PRODUCT:{
+        url:`/product/delete/:myProductId`,
+        realUrl:(myProductId)=>`/product/delete/${myProductId}`
+    }
+}
