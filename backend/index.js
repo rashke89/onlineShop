d67e23427fdf77cbec9efc79a3d05cc7cae956c5
Nex996 <neks96@gmail.com>
2022-06-30 20:29:30 +0200

    Create Contact page

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,8 @@ const serverConfig = require('./config/serverConfig');
 const app = express();
 const Product=require("./models/productModel")
 const userRouter=require('./routes/userRoute')
+const nodemailer = require("nodemailer");
+const Emails = require("./models/emailModel");
 
 
 
@@ -158,6 +160,51 @@ app.get("/api/user/",userRouter)
 
 //update user email by username
 app.use("/api/user",userRouter)
+
+
+// * CONTACT MESSAGE API CALL
+app.post('/api/send-message', async (req, res) => {
+    const reqBody = req.body;
+
+    // * ADD TO DATABASE
+    const newMessage = new Emails(reqBody);
+    const saveNewMessage = await newMessage.save();
+    // console.log(saveNewMessage);
+
+    // * NODEMAILER
+    let testAccount = await nodemailer.createTestAccount();
+
+    let transporter = nodemailer.createTransport({
+        host: "smtp.ethereal.email",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: testAccount.user, // generated ethereal user
+            pass: testAccount.pass, // generated ethereal password
+        },
+    });
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+        from: `${reqBody.firstName} ${reqBody.lastName} <${reqBody.email}>`, // sender address
+        to: "onlineShop, office@onlineShop.com", // list of receivers
+        // subject: "", // Subject line
+        // text: "Hello world?", // plain text body
+        html: `
+        <p>
+            ${reqBody.message}
+        </p>
+        `, // html body
+
+    });
+
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
+    res.send();
+});
+
+
 
 
 //Server TEST
