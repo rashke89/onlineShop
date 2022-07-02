303e6ab5e6c3cac07cbd29ce56b019cd551f64f0
Nex996 <neks96@gmail.com>
2022-07-02 16:50:43 +0200

    Refactor sort

--- frontend/src/components/FilterSort/FilterSort.js
+++ frontend/src/components/FilterSort/FilterSort.js
@@ -1,72 +1,61 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './FilterSort.scss';
 
-function FilterSort({setSort, filterStatus, setFilterStatus, filterPrice, setFilterPrice, setSearchTerm}) {
 
-	const handleSearch = e => {
-		setSearchTerm(e.target.value);
-	}
 
-	const handleInput = e => {
-		setFilterPrice(e.target.value);
-	}
+	function FilterSort({setSort, filterStatus, setFilterStatus, filterPrice, setFilterPrice, setSearchTerm}) {
 
-	const filterLayout = () => {
-		return (
-			<div className={`filter ${filterStatus ? 'filter-active' : ''}`}>
-				<div className="header">
-					<h2>Filter</h2>
-					<span onClick={() => setFilterStatus(!filterStatus)}>
+		const handleSearch = e => {
+			setSearchTerm(e.target.value);
+		}
+
+		const handleInput = e => {
+			setFilterPrice(e.target.value);
+		}
+
+		const filterLayout = () => {
+			return (
+				<div className={`filter ${filterStatus ? 'filter-active' : ''}`}>
+					<div className="header">
+						<h2>Filter</h2>
+						<span onClick={() => setFilterStatus(!filterStatus)}>
             <i className="bi bi-x"></i>
           </span>
-				</div>
-				<div className="conditions px-3">
-					<div className="row mt-5 price">
-						<label htmlFor="priceRange" className="form-label">Price: {filterPrice}</label>
-						<input type="range" onInput={handleInput} className="form-range" defaultValue="0" min="0" max="1000" step="1" id="priceRange" />
 					</div>
-					{/* <div class="row g-2">
-            <h4 className="mt-5">Price</h4>
-            <div class="col-sm">
-              <div class="form-floating">
-                <input type="number" class="form-control" id="minPrice" placeholder="Min" />
-                <label for="minPrice">Min</label>
-              </div>
-            </div>
-            <div class="col-sm">
-              <div class="form-floating">
-                <input type="number" class="form-control" id="maxPrice" placeholder="Max" />
-                <label for="maxPrice">Max</label>
-              </div>
-            </div>
-          </div> */}
-				</div>
-			</div>
-		);
-	};
-	return (
-		<>
-			<div className="d-flex justify-content-start container my-5">
-				<button className="btn filter-btn" onClick={() => setFilterStatus(!filterStatus)}>
-					<i className="bi bi-funnel me-2"></i>
-					Filter
-				</button>
-				<div className="search mx-3">
-					<form className="d-flex search h-100" role="search">
-						<div className="input-group">
-							<input className="form-control" type="search" placeholder="Search" aria-label="Search..." onChange={handleSearch} />
+					<div className="conditions px-3">
+						<div className="row mt-5 price">
+							<label htmlFor="priceRange" className="form-label">Price: {filterPrice}</label>
+							<input type="range" onInput={handleInput} className="form-range" defaultValue="0" min="0"
+								   max="1000" step="1" id="priceRange"/>
 						</div>
-					</form>
-				</div>
-				<select className="form-select sort" defaultValue="Sort by:" aria-label="Sort" onChange={(event) =>setSort(event.target.value)}>
-					<option>Sort by:</option>
-					<option value="highPrice">High price</option>
-					<option value="lowPrice">Low price</option>
-				</select>
-			</div>
-			{filterLayout()}
-		</>
-	);
-}
 
+					</div>
+				</div>
+			);
+		};
+		return (
+			<>
+				<div className="d-flex justify-content-start container my-5">
+					<button className="btn filter-btn" onClick={() => setFilterStatus(!filterStatus)}>
+						Filter
+					</button>
+					<div className="search mx-3">
+						<form className="d-flex search h-100" role="search">
+							<div className="input-group">
+								<input className="form-control" type="search" placeholder="Search" aria-label="Search"
+									   onChange={handleSearch}/>
+							</div>
+						</form>
+					</div>
+					<select className="form-select sort" defaultValue="" aria-label="Sort"
+							onChange={(event) => {setSort(event.target.value)}}>
+						<option value="lowPrice">Low price</option>
+						<option value="highPrice">High price</option>
+					</select>
+				</div>
+				{filterLayout()}
+			</>
+		);
+
+}
 export default FilterSort;
--- frontend/src/pages/Shop/Shop.js
+++ frontend/src/pages/Shop/Shop.js
@@ -5,97 +5,108 @@ import './shop.scss';
 import FilterSort from "../../components/FilterSort/FilterSort";
 
 function Shop({filterStatus, setFilterStatus}) {
-	const [ads, setAds] =useState([]);
-	const [filterPrice, setFilterPrice] = useState(0);
-	const [searchTerm, setSearchTerm] = useState("");
-	const[sort, setSort]=useState("");
-	let sortedAds;
-	let filteredAds;
-	let searchedAds;
+    const [ads, setAds] = useState([]);
+    const [filterPrice, setFilterPrice] = useState(0);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [sort, setSort] = useState("");
+    let sortedAds;
+    let filteredAds;
+    let searchedAds;
+
 
 // Search
-	useEffect(() => {
-		if(searchTerm !== "") {
-			shopService.getSearchedAds(searchTerm)
-				.then(res => {
-					if(res.status === 200) {
-						searchedAds = res.data;
-					}
+    useEffect(() => {
+        if (searchTerm !== "") {
+            shopService.getSearchedAds(searchTerm)
+                .then(res => {
+                    if (res.status === 200) {
+                        searchedAds = res.data;
+                    }
 
-					// if(searchTerm) {
-					// 	searchedAds = searchedAds.filter(ad => { return ad.title.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1})
-					// }
-					setAds(searchedAds);
 
-				})
-				.catch(err => console.log(err))
-		}
+                    setAds(searchedAds);
 
-	}, [searchTerm]);
+                })
+                .catch(err => console.log(err))
+        } else {
+            shopService.getAds()
+                .then((res) => {
+                    if (res.status === 200) {
+                        setAds(res.data)
+                    }
+                })
+                .catch(err => console.log(err));
+        }
 
-	// Sort
-	useEffect(() => {
-		shopService.getAds()
-			.then((res) => {
-				if (res.status === 200)
+    }, [searchTerm]);
 
-				sortedAds = res.data;
+    // Sort
+    useEffect(() => {
 
-				if(sort === "lowPrice"){
-					sortedAds= sortedAds.sort((a, b) => a.price - b.price);
-				}else if(sort === "highPrice"){
 
-					sortedAds= sortedAds.sort((a, b) => b.price - a.price);
-				}
-				setAds(sortedAds)
 
-				setAds(sortedAds);
 
-			})
-			.catch(err => console.log(err));
-	}, [sort]);
+        if (sort === "lowPrice") {
+            sortedAds = [...ads];
+            sortedAds = sortedAds.sort((a, b) => a.price - b.price);
+            setAds(sortedAds)
 
-	// Filter
-	useEffect(() => {
-		if(filterPrice !== 0) {
-			shopService.getFilteredAds(filterPrice)
-				.then(res => {
-					if(res.status === 200) {
-						filteredAds = res.data;
-					}
+        } else if (sort === "highPrice") {
+            sortedAds = [...ads];
+            sortedAds = sortedAds.sort((a, b) => b.price - a.price);
+            setAds(sortedAds)
 
-					// if(filterPrice > 0) {
-					// 	filteredAds = filteredAds.filter(ad => { return ad.price < parseInt(filterPrice, 10) })
-					// }
-					setAds(filteredAds);
-				})
-				.catch(err => console.log(err))
-		}
-	}, [filterPrice]);
+        }
 
-	return (
-		<div className="shop-wrapper container">
-			<div className="row">
-				<FilterSort setSort={setSort} filterStatus={filterStatus} setFilterStatus={setFilterStatus} filterPrice={filterPrice} setFilterPrice={setFilterPrice} setSearchTerm={setSearchTerm}/>
-			</div>
-			{/*<div className="shopNav d-flex justify-content-end m-auto container mt-3">*/}
 
-			{/*	<div><span>Sort by: </span>*/}
-			{/*		<select  onChange={(event)=>{*/}
-			{/*			setSort(event.target.value);*/}
+    }, [sort]);
 
-			{/*		}}>*/}
-			{/*			<option value="lowPrice">Low price</option>*/}
-			{/*			<option value="highPrice">High price</option>*/}
-			{/*		</select></div>*/}
-			{/*</div>*/}
-			<div className="row">
-				{ads.map((element) => {
-					return <ShopAd ad={element} key={element._id}/>
-				})}
-			</div>
-		</div>
-	);
+    // Filter
+    useEffect(() => {
+        if (filterPrice !== 0) {
+            shopService.getFilteredAds(filterPrice)
+                .then(res => {
+                    if (res.status === 200) {
+                        filteredAds = res.data;
+                    }
+
+                    // if(filterPrice > 0) {
+                    // 	filteredAds = filteredAds.filter(ad => { return ad.price < parseInt(filterPrice, 10) })
+                    // }
+                    setAds(filteredAds);
+                })
+                .catch(err => console.log(err))
+        }
+    }, [filterPrice]);
+
+    useEffect(() => {
+
+    }, [ads])
+
+    return (
+        <div className="shop-wrapper container">
+            <div className="row">
+                <FilterSort setSort={setSort} filterStatus={filterStatus} setFilterStatus={setFilterStatus}
+                            filterPrice={filterPrice} setFilterPrice={setFilterPrice} setSearchTerm={setSearchTerm}/>
+            </div>
+            {/*<div className="shopNav d-flex justify-content-end m-auto container mt-3">*/}
+
+            {/*	<div><span>Sort by: </span>*/}
+            {/*		<select  onChange={(event)=>{*/}
+            {/*			setSort(event.target.value);*/}
+
+            {/*		}}>*/}
+            {/*			<option value="lowPrice">Low price</option>*/}
+            {/*			<option value="highPrice">High price</option>*/}
+            {/*		</select></div>*/}
+            {/*</div>*/}
+            <div className="row">
+                {ads.map((element) => {
+                    return <ShopAd ad={element} key={element._id}/>
+                })}
+            </div>
+        </div>
+    );
 }
 
 export default Shop;
