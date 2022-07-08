a8b0fea8cbbac6562398648f0ac7134b109a7c4d
bekavacKa <th.beka17@gmail.com>
2022-07-08 18:25:25 +0200

    dynamic sticky main navbar

--- frontend/src/components/navigation/Navigation.js
+++ frontend/src/components/navigation/Navigation.js
@@ -1,26 +1,49 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import {routeConfig} from "../../config/routeConfig";
+import { Link, useNavigate } from "react-router-dom";
 import "./navigation.scss";
 
 function Navigation() {
+	const [isSticky, setIsSticky] = useState(false);
+
+	useEffect(()=>{
+		window.addEventListener("scroll", listenToScroll);
+	},[])
+
+
+	const listenToScroll = () => {
+		if(window.scrollY > 500){
+			setIsSticky("sticky-nav animate__animated animate__backInDown")
+		}
+		else{
+			setIsSticky("")
+		}
+	}
 
   return (
-	<nav className="main-nav">
+	<nav className={"main-nav " + isSticky}>
 
 		<div className="main-nav-links container">
-			<Link className="nav-link"  to="/">
+			{isSticky && 
+			<Link className="nav-link furn-logo"  to={routeConfig.HOME.url}>
+				<span>furn</span>
+				<span>home</span>
+			</Link>}
+			
+
+			<Link className="nav-link"  to={routeConfig.HOME.url}>
 				Home
 			</Link>
 
-			<Link className="nav-link" to="/about">
+			<Link className="nav-link" to={routeConfig.ABOUT.url}>
 				About
 			</Link>
 
-			<Link className="nav-link" to="/shop">
+			<Link className="nav-link" to={routeConfig.SHOP.url}>
 				Shop
 			</Link>
 
-			<Link className="nav-link" to="/contact">
+			<Link className="nav-link" to={routeConfig.CONTACT.url}>
 				Contact
 			</Link>
 		</div>
