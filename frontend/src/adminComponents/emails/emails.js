ee3749d75df55b441a563a41c02ba81849991294
andrijadenic9 <andrijadenic9@gmail.com>
2022-07-13 23:55:12 +0200

    emails page done

--- frontend/src/adminComponents/emails/emails.js
+++ frontend/src/adminComponents/emails/emails.js
@@ -1,11 +1,48 @@
 import { useEffect, useState } from "react";
 import MessageService from "../../services/messageService";
 import { ToastContainer, toast } from "react-toastify";
-import "./emails.scss";
+import Modal from 'react-modal';
+
+const popup = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: '50%',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    boxShadow: '0 1px 8px rgba(0, 0, 0, 0.3)',
+  },
+  overlay: {
+    position: 'absolute',
+    backgroundColor: '#0e0e0ead'
+  },
+  heading: {
+    marginBottom: '30px'
+  },
+  div: {
+    display: 'flex',
+    justifyContent: 'space-between'
+  },
+  cancel: {
+    padding: '7px 15px',
+    backgroundColor: 'green',
+    border: '1px solid green',
+    color: 'white'
+  },
+  delete: {
+    padding: '7px 15px',
+    backgroundColor: 'tomato',
+    border: '1px solid tomato',
+    color: 'white'
+  }
+}
 
 function Emails() {
   const [emailsInfo, setEmailsInfo] = useState([]);
+  const [messageID, setMessageID] = useState('');
   const [isDone, setIsDone] = useState(false);
+  const [isModal, setIsModal] = useState(false);
 
   useEffect(() => {
     MessageService.getAllMessages()
@@ -13,9 +50,29 @@ function Emails() {
         setEmailsInfo(res.data);
       })
       .catch((err) => {
-        console.log(err, "emails front");
+        console.log(err);
       });
   }, [isDone]);
+
+
+  const checkLength = (message) => {
+    if (message.length > 100) {
+      return (
+        <td style={{ cursor: 'pointer' }} onClick={e => { toggleMessage(e, message) }}>{`${message.substring(0, 100)} ...READ MORE...`}</td>
+      )
+    } else {
+      return <td>{message}</td>;
+    }
+  }
+  const toggleMessage = (e, message) => {
+    if (e.target.innerHTML === `${message.substring(0, 100)} ...READ MORE...`) {
+      console.log('KRATAK SAM');
+      e.target.innerHTML = `${message} ...READ LESS...`;
+    } else if (e.target.innerHTML === `${message} ...READ LESS...`) {
+      console.log('dugacak sam');
+      e.target.innerHTML = `${message.substring(0, 100)} ...READ MORE...`;
+    }
+  }
 
   const messageLayout = () => {
     return emailsInfo.map((item, index) => {
@@ -25,11 +82,13 @@ function Emails() {
           <td>{item.firstName}</td>
           <td>{item.lastName}</td>
           <td>{item.email}</td>
-          <td>{item.message}</td>
+          {checkLength(item.message, index)}
           <td>
             <button
               className="btn-danger"
-              onClick={(e) => deleteMessage(item._id)}
+              onClick={(e) => {
+                openModal(item._id)
+              }}
             >
               Delete
             </button>
@@ -39,22 +98,29 @@ function Emails() {
     });
   };
 
+  const openModal = (id) => {
+    setIsModal(true)
+    setMessageID(id);
+  }
+
   const deleteMessage = (id) => {
     MessageService.deleteMessage(id)
       .then((res) => {
         setIsDone(!isDone);
-        toast.success("Message deleted");
+        toast.success("Message successfully deleted");
       })
       .catch((err) => {
         console.log(err);
         toast.error("Something went wrong");
-      });
+      })
+      .finally(() => {
+        setIsModal(false);
+      })
   };
+
   return (
     <>
       <h1>Emails</h1>
-      <ToastContainer />
-
       <table className="table table-striped table-dark">
         <thead>
           <tr>
@@ -63,11 +129,24 @@ function Emails() {
             <th scope="col">Last Name</th>
             <th scope="col">Email</th>
             <th scope="col">Message</th>
-            <th scope="col">Delete</th>
+            <th scope="col"></th>
           </tr>
         </thead>
         <tbody>{messageLayout()}</tbody>
       </table>
+
+      {isModal &&
+        <Modal isOpen={true} ariaHideApp={false} style={popup} aria-labelledby='contained-modal-title-vcenter'
+          centered>
+          <h3 style={popup.heading}>Delete message</h3>
+          <div style={popup.div}>
+            <button style={popup.cancel} onClick={e => setIsModal(false)}>Cancel</button>
+            <button style={popup.delete} onClick={e => deleteMessage(messageID)}>Delete</button>
+          </div>
+        </Modal>
+      }
+
+      <ToastContainer />
     </>
   );
 }
