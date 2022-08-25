14e05ac2f3b4770ff0852437308850f5a370c45c
peratrusnik <40424206+peratrusnik@users.noreply.github.com>
2022-08-25 18:55:27 +0200

    Delete serverConfig.js

--- backend/config/serverConfig.js
+++
@@ -1,8 +0,0 @@
-const port = 4000
-const ServerConfig = {
-  port: port,
-  serverRunningMsg: `Server is running on port: ${port}`.cyan.bold,
-  link: `http://localhost:${port}`,
-}
-
-module.exports = ServerConfig
